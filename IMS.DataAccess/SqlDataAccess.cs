19f2c25b863f471708b28742162c0a74ea8c5d97
Newanga Wickramasinghe <newanga@live.com>
2020-07-01 18:31:08 +0530

    Add main Data Access Class.

---
+++ IMS.DataAccess/SqlDataAccess.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.DataAccess
+{
+    public class SqlDataAccess
+    {
+        public List<T> LoadData<T, U>(string sqlStatement, U parameters, string connectionString)
+        {
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(sqlStatement, parameters).ToList();
+                return rows;
+            }
+        }
+
+        public void SaveData<T>(string sqlStatement, T parameters, string connectionString)
+        {
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Execute(sqlStatement, parameters);
+            }
+        }
+    }
+}
