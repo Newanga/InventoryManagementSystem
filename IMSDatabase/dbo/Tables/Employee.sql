e98bde990920e539aaddb9583b0a40a46abe25c6
Newanga Wickramasinghe <newanga@live.com>
2020-07-08 14:32:59 +0530

    Add Employee Table changes.

--- IMSDatabase/dbo/Tables/Employee.sql
+++ IMSDatabase/dbo/Tables/Employee.sql
@@ -5,7 +5,8 @@
     [LastName] NVARCHAR(100) NOT NULL, 
     [DateOfBirth] DATE NOT NULL, 
     [Address] NVARCHAR(250) NOT NULL, 
-    [JoinedDate] DATE NOT NULL, 
+    [StartDate] DATE NOT NULL,
+    [LeaveDate] DATE NULL, 
     [AccountId] INT NOT NULL, 
     CONSTRAINT [FK_Employee_Account] FOREIGN KEY (AccountId) REFERENCES [Account](Id)
 )
