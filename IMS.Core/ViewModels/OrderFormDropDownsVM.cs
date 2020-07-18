cda1ca2edcfa04639527b63190014813fe76ddf5
Newanga Wickramasinghe <newanga@live.com>
2020-07-18 17:36:32 +0530

    Code Refactor.

---
+++ IMS.Core/ViewModels/OrderFormDropDownsVM.cs
@@ -0,0 +1,18 @@
+﻿using IMS.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Core.ViewModels
+{
+    public class OrderFormDropDownsVM
+    {
+        public List<SupplierName> Suppliers { get; set; }
+
+        public List<SupplierProductsModel> Products { get; set; }
+
+
+    }
+}
